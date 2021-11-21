8a775188135c517aefcf826cfb3c791c0db2666d
robertonav20 <roberto.nav20@gmail.com>
2021-11-21 01:51:24 +0100

    Update strimzi.sh

--- yaml/strimzi/strimzi.sh
+++ yaml/strimzi/strimzi.sh
@@ -1,2 +1,2 @@
 kubectl create -f 'https://strimzi.io/install/latest?namespace=default' -n default
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n default
+kubectl apply -f kafka-persistence.yaml
