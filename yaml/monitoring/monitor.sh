638e4396322d201180a5088040a05458246bf420
roberto.nav20 <roberto.nav20@gmail.com>
2021-12-10 03:00:35 +0100

    bugfix grafana

--- yaml/monitoring/monitor.sh
+++ yaml/monitoring/monitor.sh
@@ -1,6 +1,7 @@
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/v0.40.0/bundle.yaml
 kubectl apply -f monitoring-prometheus.yaml
 kubectl apply -f monitoring-prometheus-monitor.yaml
+kubectl apply -f monitoring-prometheus-internal.yaml
 kubectl apply -f monitoring-prometheus-external.yaml
 kubectl apply -f monitoring-grafana.yaml
 kubectl apply -f monitoring-camel-k-service-internal.yaml
