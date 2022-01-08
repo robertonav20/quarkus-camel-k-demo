7aabb2cd8a7677c3ce9e6fe0d2da5b9401e6e680
roberto.nav20 <roberto.nav20@gmail.com>
2022-01-08 14:40:44 +0100

    remove useless import

--- quarkus-web-socket-server/src/main/java/demo/com/WebSocketServer.java
+++ quarkus-web-socket-server/src/main/java/demo/com/WebSocketServer.java
@@ -6,8 +6,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/sessions/{component}")
 @ApplicationScoped
