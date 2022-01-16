48e366fa1b095c24b228aabb58a820b26183116b
roberto.nav20 <roberto.nav20@gmail.com>
2022-01-16 17:22:02 +0100

    add quarkus yaml

--- yaml/quarkus/quarkus.sh
+++ yaml/quarkus/quarkus.sh
@@ -1,2 +1,5 @@
 kubectl apply -f quarkus-web-socket-server-ingress.yaml
-kubectl apply -f quarkus-camel-k-demo-role.yaml
+kubectl apply -f quarkus-web-socket-server-service.yaml
+kubectl apply -f quarkus-web-socket-server.yaml
+kubectl apply -f quarkus-camel-k-demo-config.yaml
+kubectl apply -f quarkus-camel-k-demo.yaml
