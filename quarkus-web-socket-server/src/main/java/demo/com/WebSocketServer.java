76c48bf803bb6903aacf5f699e27744b971c85a6
roberto.nav20 <roberto.nav20@gmail.com>
2022-02-13 22:36:33 +0100

    bugfix WebSocketServer logic

--- quarkus-web-socket-server/src/main/java/demo/com/WebSocketServer.java
+++ quarkus-web-socket-server/src/main/java/demo/com/WebSocketServer.java
@@ -21,71 +21,83 @@ public class WebSocketServer {
 
     @OnOpen
     public void onOpen(Session session, @PathParam("component") String component) throws JsonProcessingException {
+        logger.info("Received an open message from component " + component + "!");
         if (component.equalsIgnoreCase("frontend")) {
             frontedSession = session;
-            if (backendSession != null) {
-                WebSocketServerEvent event = WebSocketServerEvent.builder()
-                    .type("START")
-                    .component("backend")
-                    .message("The backend join!")
-                    .build();
-                broadcast(this.objectMapper.writeValueAsString(event), session);
-            }
         } else if (component.equalsIgnoreCase("backend")) {
             backendSession = session;
+        }
+
+        if (frontedSession != null && backendSession != null) {
+            WebSocketServerEvent event = WebSocketServerEvent.builder()
+                .type("START")
+                .component("backend")
+                .message("The backend join!")
+                .build();
+            broadcast("frontend", this.objectMapper.writeValueAsString(event), session);
         }
     }
 
     @OnClose
     public void onClose(Session session, @PathParam("component") String component) throws JsonProcessingException {
-        logger.info("Received a close message from component " + component + " left!");
+        logger.info("Received a close message from component " + component + "!");
+        String destination = null;
         if (component.equalsIgnoreCase("frontend")) {
             frontedSession = null;
             session = backendSession;
+            destination = "backend";
         } else if (component.equalsIgnoreCase("backend")) {
             backendSession = null;
             session = frontedSession;
+            destination = "frontend";
         }
         WebSocketServerEvent event = WebSocketServerEvent.builder()
             .type("STOP")
             .component(component)
             .message("The " + component + " left!")
             .build();
-        broadcast(this.objectMapper.writeValueAsString(event), session);
+        broadcast(destination, this.objectMapper.writeValueAsString(event), session);
     }
 
     @OnError
     public void onError(Session session, @PathParam("component") String component, Throwable throwable) throws JsonProcessingException {
-        logger.info("Received an error message from component " + component + " left!");
+        logger.info("Received an error message from component " + component + "!");
+        String destination = null;
         if (component.equalsIgnoreCase("frontend")) {
             frontedSession = null;
             session = backendSession;
+            destination = "backend";
         } else if (component.equalsIgnoreCase("backend")) {
             backendSession = null;
             session = frontedSession;
+            destination = "frontend";
         }
         WebSocketServerEvent event = WebSocketServerEvent.builder()
                 .type("STOP")
                 .component(component)
                 .message("The " + component + " left with error!")
                 .build();
-        broadcast(this.objectMapper.writeValueAsString(event), session);
+        broadcast(destination, this.objectMapper.writeValueAsString(event), session);
     }
 
     @OnMessage
     public void onMessage(String message, @PathParam("component") String component) {
         logger.info("Received a message : " + message);
         Session session = null;
+        String destination = null;
         if (component.equalsIgnoreCase("frontend")) {
             session = backendSession;
+            destination = "backend";
         } else if (component.equalsIgnoreCase("backend")) {
             session = frontedSession;
+            destination = "frontend";
         }
-        broadcast(message, session);
+        broadcast(destination, message, session);
     }
 
-    private void broadcast(String message, Session session) {
+    private void broadcast(String component, String message, Session session) {
         if (session != null) {
+            logger.info("Sending message to " + component);
             session.getAsyncRemote().sendObject(message, result -> {
                 if (result.getException() != null) {
                     logger.info("Unable to send message: " + result.getException());
