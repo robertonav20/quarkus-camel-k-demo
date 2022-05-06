47a0b60cd4094cdb63b22e2977274405c273ba75
rob <roberto.nav20@gmail.com>
2022-05-06 02:39:30 +0200

    sonar lint correction

--- quarkus-camel-parent/event-generator-timer/src/main/java/event/generator/timer/Generator.java
+++ quarkus-camel-parent/event-generator-timer/src/main/java/event/generator/timer/Generator.java
@@ -1,3 +1,5 @@
+package event.generator.timer;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.camel.Processor;
@@ -9,55 +11,58 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Date;
 import java.util.UUID;
+import java.util.Random;
 
 @ApplicationScoped
-public class EventGenerator extends RouteBuilder {
+public class Generator extends RouteBuilder {
 	private final Gson gson = new Gson();
+	private final Random random = new Random();
+	private static final String EVENT_TYPE_KEY = "eventType";
+	private static final String LOG_ROUTE = "seda:a";
 
 	@ConfigProperty(name = "route.id")
 	String name;
 
 	@Override
 	public void configure() {
-		from("seda:a")
+		from(LOG_ROUTE)
 			.log("${body}");
 
 		from("timer:clock?period={{timer.period}}")
 			.routeId(name)
-			.setHeader("eventType").message(message -> generateType())
+			.setHeader(EVENT_TYPE_KEY).message(message -> generateType())
 			.log("Generated event type ${header.eventType}")
 			.choice()
 			.when(simple("${header.eventType} < 3"))
 			.process(getOpenTracingProcessor())
-			.setBody().message(message -> generateEvent((String) message.getHeader("eventType")))
+			.setBody().message(message -> generateEvent((String) message.getHeader(EVENT_TYPE_KEY)))
 			.log("Send to event.topic.main ${body}")
-			.to("seda:a")
+			.to(LOG_ROUTE)
 			//.to("paho-mqtt5:event.topic.main?brokerUrl=tcp://mosquitto:1883")
 			.when(simple("${header.eventType} >= 3 && ${header.eventType} < 7"))
 			.process(getOpenTracingProcessor())
-			.setBody().message(message -> generateEvent((String) message.getHeader("eventType")))
+			.setBody().message(message -> generateEvent((String) message.getHeader(EVENT_TYPE_KEY)))
 			.log("Send to event.topic.secondary ${body}")
-			.to("seda:a")
+			.to(LOG_ROUTE)
 			//.to("paho-mqtt5:event.topic.secondary?brokerUrl=tcp://mosquitto:1883")
 			.otherwise()
 			.process(getOpenTracingProcessor())
-			.setBody().message(message -> generateEvent((String) message.getHeader("eventType")))
+			.setBody().message(message -> generateEvent((String) message.getHeader(EVENT_TYPE_KEY)))
 			.log("Send to event.topic.others ${body}")
-			.to("seda:a");
+			.to(LOG_ROUTE);
 			//.to("paho-mqtt5:event.topic.others?brokerUrl=tcp://mosquitto:1883");
 
 	}
 
 	private String generateType() {
-		int random = (int) (Math.random() * 10);
-		return String.valueOf(random);
+		return String.valueOf(this.random.nextInt(10));
 	}
 
 	private String generateEvent(String type) {
 		JsonObject event = new JsonObject();
 		event.addProperty("id", UUID.randomUUID().toString());
 		event.addProperty("date", new Date().toString());
-		event.addProperty("name", "EVENT_" + ((int) (Math.random() * 10000)));
+		event.addProperty("name", "EVENT_" + this.random.nextInt(100000));
 		event.addProperty("type", type);
 
 		return gson.toJson(event);
